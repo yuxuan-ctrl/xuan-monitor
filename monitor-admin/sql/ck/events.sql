51556c83849da97dd7928aa0a737baacaf2822f9
yuxuan-ctrl <714180720@qq.com>
2024-03-04 23:45:17 +0800

    feat: SQL修改

--- monitor-admin/sql/ck/events.sql
+++ monitor-admin/sql/ck/events.sql
@@ -1,27 +1,22 @@
-CREATE TABLE events (
-                        appId String,
-                        mostVisitedPageId String,
-                        mostVisitedPageViews Int32,
-                        pageUrl String,
-                        userAgent String,
-                        platform String,
-                        screenResolution Nested (key String, value Int32),
-                        timestamp DateTime64(3), -- Assuming Timestamp is in milliseconds and should be stored as DateTime with precision
-                        referrer String,
-                        uniqueKey String,
-                        language String,
-                        timeZoneOffset Int32,
-                        name String,
-                        userId String,
-                        stayDuration Float64,
-                        createTime DateTime, -- Assuming this is a regular date/time field
-                        metrics Nested (key String, value String), -- Depending on the actual types of values, you may need to use other types
-                        slowResources Nested (
-                            resource Array(Nested(
-                            key String,
-                            value Nested(key String, value String)
-                            ))
-                            )
+CREATE TABLE event_info (
+                            app_id String,
+                            most_visited_page_id String,
+                            most_visited_page_views Int32,
+                            page_url String,
+                            user_agent String,
+                            platform String,
+                            screen_resolution String, -- JSON format
+                            timestamp DateTime64(3),
+                            referrer String,
+                            unique_key String,
+                            language String,
+                            time_zone_offset Int32,
+                            name String,
+                            user_id String,
+                            stay_duration Float64,
+                            create_time DateTime,
+                            metrics String, -- JSON format
+                            slow_resources String, -- JSON format
 ) ENGINE = MergeTree()
-PARTITION BY toDate(createTime) -- Optionally partition by date if that's relevant for your use case
-ORDER BY (createTime);
+PARTITION BY toStartOfDay(create_time)
+ORDER BY (create_time);
